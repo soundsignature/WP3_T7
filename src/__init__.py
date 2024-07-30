14dbb52f7858a82932512eb122a5d7c23105a57c
Jose Antonio <joseantoniogarcia@ctnaval.com>
2024-07-30 12:43:10 +0200

    Added poetry and __init__ in src for poetry to work

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 30 12:41:45 2024
+
+@author: Jose Antonio
+"""
+
+# For poetry to recognize the project as a package
+
+from src.dataset import EcossDataset
