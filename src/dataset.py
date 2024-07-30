3ed3e5147f06501c0827cc5405a5d170cd825ccf
Jose Antonio <joseantoniogarcia@ctnaval.com>
2024-07-30 09:37:59 +0200

    Function for plots done, todo added and also renamed the function of the onthology to make the remapping optional

--- src/dataset.py
+++ src/dataset.py
@@ -10,6 +10,7 @@ Created on Mon Jul 29 13:27:48 2024
 
 import pandas as pd
 import os
+from matplotlib import pyplot as plt
 from dotenv import load_dotenv
 
 UNWANTED_LABELS = ["Undefined", "Anthropogenic"]
@@ -57,15 +58,17 @@ class EcossDataset:
         self.df.reset_index(drop=True, inplace=True)
         
     
-    def remap_onthology(self, labels: list[str]):
+    def fix_onthology(self, labels: list[str] = None):
         """
         This function generates a new column with the final labels on
-        the annotations.csv file.
-
+        the annotations.csv file. If a list of labels is used, the function
+        not only formats the labels to use the last part, but also fixes the
+        onthology to the level of detail requested.
+        
         Parameters
         ----------
-        labels : list[str]
-            A list with the labels to be modified.
+        labels : list[str], optional
+            A list with the labels to be modified.. The default is None.
 
         Returns
         -------
@@ -76,12 +79,13 @@ class EcossDataset:
         self.df = self.df.dropna(subset=['label_source'])
         self.df.reset_index(drop=True, inplace=True)
         
-        for i, row in self.df.iterrows():
-            for label in labels:
-                idx = row["label_source"].find(label)
-                if idx != -1:
-                    delimiter = idx + len(label)
-                    self.df.loc[i, "label_source"] = self.df.loc[i, "label_source"][:delimiter]
+        if labels != None:
+            for i, row in self.df.iterrows():
+                for label in labels:
+                    idx = row["label_source"].find(label)
+                    if idx != -1:
+                        delimiter = idx + len(label)
+                        self.df.loc[i, "label_source"] = self.df.loc[i, "label_source"][:delimiter]
         
         # Once they are defined, we create the final column with the labels
         # Currently not saving, only overwritting the df parameter as this is the first step
@@ -90,9 +94,49 @@ class EcossDataset:
         # Now, we can proceed to eliminate the unwanted labels
         self.drop_unwanted_labels(UNWANTED_LABELS)
         
-                    
+        
+    # TODO: When the splitting is performed, go for metrics per split
     def generate_insights(self):
-        pass
+        """
+        This function is used to generate insights on the data. It generates plots
+        for the number of sound signatures per class, and the time per class.
+        
+        IMPORTANT: It needs to be used right after the first step (the remapping and reformating of classes).
+        However, you dont need to remap the labels if you don't want to, as this parameter
+        is optional.
+
+        Returns
+        -------
+        None.
+
+        """
+        # Plot for number of sound signatures (time independent)
+        count_signatures = self.df["final_source"].value_counts()
+        plt.figure(figsize=(8,6))
+        plt.bar(range(0, len(count_signatures)), count_signatures)
+        plt.xticks(range(0, len(count_signatures)),
+                   count_signatures.index.to_list(),
+                   horizontalalignment='center')
+        plt.xlabel("Source")
+        plt.ylabel("# of sound signatures")
+        plt.show()
+        
+        # Plot for time per class of sound signature
+        times = dict()
+        for i, row in self.df.iterrows():
+            if row["final_source"] not in times.keys():
+                times[row["final_source"]] = row["tmax"] - row["tmin"]
+            else:
+                times[row["final_source"]] += row["tmax"] - row["tmin"]
+        plt.figure(figsize=(8,6))
+        plt.bar(range(0, len(times)), times.values())
+        plt.xticks(range(0, len(times)),
+                   list(times.keys()),
+                   horizontalalignment='center')
+        plt.xlabel("Source")
+        plt.ylabel("Time (s)")
+        plt.show()
+        
 
     
 if __name__ == "__main__":
@@ -100,8 +144,8 @@ if __name__ == "__main__":
     ANNOTATIONS_PATH = os.getenv("ANNOTATIONS_PATH")
     # LABELS = 
     ecoss_data = EcossDataset(ANNOTATIONS_PATH, '.', '.', '.', '.')
-    ecoss_data.remap_onthology(["Odontocetes", "Ship"])
-    ecoss_data.df.final_source
+    ecoss_data.fix_onthology(labels=["Odontocetes", "Ship"])
+    times = ecoss_data.generate_insights()
     
     
     
