eb1f66cd077e27e0697d164ab23877f82433b9c9
Pablo Aguirre Cárcel <p.aguirre@electronica-submarina.com>
2024-08-02 12:28:38 +0200

    added train.py script example

--- src/pipeline/passt_model.py
+++ src/pipeline/passt_model.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+
+
 """
 Created on Fri Aug 02 10:10:48 2024
 
@@ -8,22 +10,34 @@ Created on Fri Aug 02 10:10:48 2024
 """ This script implements the class PasstModel which is responsible for all the training, testing and inference stuff related with the
     PaSST model """
 
+import yaml
+from pathlib import Path
+import logging
 class PasstModel():
     def __init__(self, yaml_content: dict, data_path: str) -> None:
         self.yaml = yaml_content
         self.data_path = data_path
+        
+        self.data_path = Path(self.data_path)
+        
 
+    
+    def train(self,results_folder):
+        # Example 
+        self.results_folder = Path(results_folder)
+        logging.info(f"Training PASST")
+        output_config_path = self.results_folder / 'configuration.yaml'
+        logging.info(f"Saving configuration in {output_config_path}")
+        with open(str(output_config_path), 'w') as outfile:
+            yaml.dump(self.yaml, outfile, default_flow_style=False)
+        logging.info(f"Config params:\n {self.yaml}")
+        
 
-
-    def train(self):
+    def test(self,results_folder):
         pass
 
 
-    def test(self):
-        pass
-
-
-    def inference(self):
+    def inference(self,results_folder):
         pass
 
 
--- src/pipeline/utils.py
+++ src/pipeline/utils.py
@@ -1,6 +1,7 @@
 """ Script for useful functions """
 
 import yaml
+from pathlib import Path
 
 def load_yaml(yaml_path: str) -> dict:
     """Function used to load the yaml content. Useful for reading configuration files or any other data stored in YAML format.
@@ -18,3 +19,26 @@ def load_yaml(yaml_path: str) -> dict:
         except yaml.YAMLError as e:
             print(e)
             return None
+        
+
+def create_exp_dir(name: str, model: str, task: str) -> str:
+    """
+    Function to create a unique experiment directory. Useful for organizing experiment results by model and task.
+
+    Args:
+        name (str): The base name for the experiment directory.
+        task (str): The name of the task associated with the experiment.
+        model (str): The name of the model associated with the experiment.
+
+    Returns:
+        str: The path to the created experiment directory.
+    """
+    parent_path = Path(f'runs/{model}/{task}')
+    parent_path.mkdir(exist_ok=True, parents=True)
+    exp_path = str(parent_path / name) + "_{:02d}"
+    i = 0
+    while Path(exp_path.format(i)) in list(parent_path.glob("*")):
+        i += 1
+    exp_path = Path(exp_path.format(i))
+    exp_path.mkdir(exist_ok=True)
+    return str(exp_path)
