3ed36e52e91a0c0a6393b041254fa30a3440c36b
Jose Antonio <joseantoniogarcia@ctnaval.com>
2024-08-02 10:57:05 +0200

    added structures, utils.py and modified poetry to include pyyaml

---
+++ src/pipeline/utils.py
@@ -0,0 +1,20 @@
+""" Script for useful functions """
+
+import yaml
+
+def load_yaml(yaml_path: str) -> dict:
+    """Function used to load the yaml content. Useful for
+
+    Args:
+        yaml_path (str): The absolute path to the yaml
+
+    Returns:
+        dict: The yaml content
+    """
+    with open(yaml_path, 'r') as file:
+        try:
+            yaml_content = yaml.safe_load(file)
+            return yaml_content
+        except yaml.YAMLError as e:
+            print(e)
+            return None
---
+++ src/pipeline/vggish_model.py
@@ -0,0 +1,42 @@
+"""
+Created on Fri Aug 02 10:10:48 2024
+
+@authors: Pablo Aguirre, Isabel Carozzo, Jose Antonio García, Mario Vilar
+"""
+
+""" This script implements the class VggishModel which is responsible for all the training, testing and inference stuff related with the
+    Vggish + SVM model """
+
+class VggishModel():
+    def __init__(self, yaml_content: dict, data_path: str) -> None:
+        self.yaml = yaml_content
+        self.data_path = data_path
+
+
+
+    def train():
+        pass
+
+
+    def test():
+        pass
+
+
+    def inference():
+        pass
+
+
+    def plot_results():
+        pass
+
+
+    def read_yaml_file():
+        pass
+
+
+    def save_weights():
+        pass
+
+
+    def plot_processed_data():
+        pass
